21e959800326cfff73df5d25730d7acee57449a1
andrea <andrea@email.domain>
2020-04-21 00:02:45 +0200

    build

---
+++ precache-manifest.5be609408080a8b8dd842043c42e6a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9439ec34f0baa1487f3746b5704a45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f71c666bf06a7b709408",
+    "url": "/static/css/main.f7ef3787.chunk.css"
+  },
+  {
+    "revision": "47ff8b6da7280f0c6b24",
+    "url": "/static/js/2.bc54fe3b.chunk.js"
+  },
+  {
+    "revision": "96753f42ddefada5f15056d068af57c9",
+    "url": "/static/js/2.bc54fe3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f71c666bf06a7b709408",
+    "url": "/static/js/main.87857a75.chunk.js"
+  },
+  {
+    "revision": "933cbc17ad372c7b2e22",
+    "url": "/static/js/runtime-main.a7ad252f.js"
+  }
+]);
