662cf232c5d868b814b188d3c5f4bb3b9fae69e6
andrea <andrea@email.domain>
2020-04-21 00:26:21 +0200

    forgot to add files

---
+++ precache-manifest.cc7443e85eb8c260c5c8480f6ff8f750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c764e5453c853687ee4b46614504d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45e16ea24bea539948a6",
+    "url": "/static/css/main.f7ef3787.chunk.css"
+  },
+  {
+    "revision": "47ff8b6da7280f0c6b24",
+    "url": "/static/js/2.bc54fe3b.chunk.js"
+  },
+  {
+    "revision": "96753f42ddefada5f15056d068af57c9",
+    "url": "/static/js/2.bc54fe3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45e16ea24bea539948a6",
+    "url": "/static/js/main.1b272111.chunk.js"
+  },
+  {
+    "revision": "933cbc17ad372c7b2e22",
+    "url": "/static/js/runtime-main.a7ad252f.js"
+  }
+]);
