a369aaecb9ccb7a034687f7e472cfc23464fb4ed
andrea <andrea@email.domain>
2020-04-22 09:43:05 +0200

    natural gr definition

---
+++ precache-manifest.3e0afe6fea3fef42c34f680a40b6b732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d92f4f1f4ff3cf5f3787644eb26071c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "376290f79c2313bd1443",
+    "url": "/static/css/main.f7ef3787.chunk.css"
+  },
+  {
+    "revision": "452a8a951c2882dfa2d2",
+    "url": "/static/js/2.37f7b1a4.chunk.js"
+  },
+  {
+    "revision": "96753f42ddefada5f15056d068af57c9",
+    "url": "/static/js/2.37f7b1a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "376290f79c2313bd1443",
+    "url": "/static/js/main.077df08c.chunk.js"
+  },
+  {
+    "revision": "933cbc17ad372c7b2e22",
+    "url": "/static/js/runtime-main.a7ad252f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.462c20ff5ad96585b7f80fcb97989b6b.js"
+  "/precache-manifest.3e0afe6fea3fef42c34f680a40b6b732.js"
 );
 
 self.addEventListener('message', (event) => {
