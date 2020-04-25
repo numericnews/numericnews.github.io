135a6b410f3f80d1ce93de3afd3c6cf026802f0b
andrea <andrea@email.domain>
2020-04-25 10:32:26 +0200

    added files

---
+++ precache-manifest.d5f81de3dde9aef2b0aaeee7e80eb21b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7ababe020efed883b6e448f37a9005",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a3332109ef97d8c672b",
+    "url": "/static/css/main.eacc87ee.chunk.css"
+  },
+  {
+    "revision": "6166560079a725950f87",
+    "url": "/static/js/2.bf67299f.chunk.js"
+  },
+  {
+    "revision": "96753f42ddefada5f15056d068af57c9",
+    "url": "/static/js/2.bf67299f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a3332109ef97d8c672b",
+    "url": "/static/js/main.06fc690e.chunk.js"
+  },
+  {
+    "revision": "933cbc17ad372c7b2e22",
+    "url": "/static/js/runtime-main.a7ad252f.js"
+  }
+]);
